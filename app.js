f550ac7f1ff0c71fd588061ea6f2a51c0cbf4912
kdswle <kanta_shouno@dwango.co.jp>
2023-05-17 14:02:23 +0900

    renderの環境変数を利用できるように修正

--- app.js
+++ app.js
@@ -25,8 +25,10 @@ User.sync().then(async () => {
 });
 
 const GitHubStrategy = require('passport-github2').Strategy;
-const GITHUB_CLIENT_ID = '2f831cb3d4aac02393aa';
-const GITHUB_CLIENT_SECRET = '9fbc340ac0175123695d2dedfbdf5a78df3b8067';
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || '2f831cb3d4aac02393aa';
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || '9fbc340ac0175123695d2dedfbdf5a78df3b8067';
+// const GITHUB_CLIENT_ID = '2f831cb3d4aac02393aa';
+// const GITHUB_CLIENT_SECRET = '9fbc340ac0175123695d2dedfbdf5a78df3b8067';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
